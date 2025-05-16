86fae2e232f70fb1678a506501024b748db6e3ff
badayelme <olegrakovice355@gmail.com>
2025-05-16 14:13:33 +0700

    task 2.5

--- LABA 2/Task 5.py
+++ LABA 2/Task 5.py
@@ -1,18 +1,38 @@
 class Class:
     def __init__(self, name="NoName"):
-        self.name = name
-        print(f"Объект {self.name} создан")
+        self._name = name
+        print(f"Объект {self._name} создан")
 
     def __del__(self):
-        print(f"Объект {self.name} удален")
+        print(f"Объект {self._name} удален")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
 
 while True:
-    object = input("Создать объект? (Да/Нет): ").lower()
-    if object == 'да':
-        example = Class()
+    choice = input("Создать объект? (Да/Нет): ")
+    if choice == 'Да':
+        name = input("Введите имя объекта: ")
+        example = Class(name)
         break
-    elif object == 'нет':
+    elif choice == 'Нет':
         print("OK")
+        break
+    else:
+        print("Error")
+while True:
+    choice_2 = int(input("1 - Редактировать обьект\n2 - Удалить обьект\nДействие: "))
+    if choice_2 == 1:
+            new_name = input("Введите новое имя объекта: ")
+            example.name = new_name
+            print(f"Имя объекта изменено на {example.name}")
+    elif choice_2 == 2:
+        del example
         break
     else:
         print("Error")
