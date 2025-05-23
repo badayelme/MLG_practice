cd3ca4c4eda90e19e9b42ff25d1009b31cbdec22
badayelme <olegrakovice355@gmail.com>
2025-05-24 03:05:41 +0700

    task 4.1

--- LABA 4/Task 1.py
+++ LABA 4/Task 1.py
@@ -8,64 +8,57 @@ class Student:
         self.group = group
         self.grades = grades
 
-class Database:
-    def __init__(self):
-        self.con = sqlite3.connect('students.db')
-        self.cursor = self.con.cursor()
-        self.create_table()
+con = sqlite3.connect('students.db')
+cursor = con.cursor()
 
-    def create_table(self):
-        self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS students (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                first_name TEXT NOT NULL,
-                last_name TEXT NOT NULL,
-                middle_name TEXT NOT NULL,
-                group_name TEXT NOT NULL,
-                grades INTEGER NOT NULL
-            )
-        """)
-        self.con.commit()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        middle_name TEXT NOT NULL,
+        group_name TEXT NOT NULL,
+        grades TEXT NOT NULL
+    )
+""")
+con.commit()
 
-    def add_student(self, student):
-        grades_str = ','.join(map(str, student.grades))
-        self.cursor.execute('''
-                    INSERT INTO students (first_name, last_name, middle_name, group_name, grades)
-                    VALUES (?, ?, ?, ?, ?)
-                ''', (student.first_name, student.last_name, student.middle_name, student.group, grades_str))
-        self.con.commit()
+def add_student(student):
+    grades_str = ','.join(map(str, student.grades))
+    cursor.execute('''
+                INSERT INTO students (first_name, last_name, middle_name, group_name, grades)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (student.first_name, student.last_name, student.middle_name, student.group, grades_str))
+    con.commit()
 
-    def get_all_students(self):
-        self.cursor.execute('SELECT * FROM students')
-        return self.cursor.fetchall()
+def get_all_students():
+    cursor.execute('SELECT * FROM students')
+    return cursor.fetchall()
 
-    def get_student(self, student_id):
-        self.cursor.execute('SELECT * FROM students WHERE id = ?', (student_id,))
-        student_information = self.cursor.fetchone()
-        grades = list(map(int, student_information[5].split(',')))
-        return print(f"id: {student_information[0]}\nИмя: {student_information[1]}\nФамилия: {student_information[2]}\nОтчество: {student_information[3]}\nГруппа: {student_information[4]}\nОценки: {grades}\nСредний балл: {sum(grades) / len(grades)}")
+def get_student(student_id):
+    cursor.execute('SELECT * FROM students WHERE id = ?', (student_id,))
+    student_information = cursor.fetchone()
+    grades = list(map(int, student_information[5].split(',')))
+    return f"id: {student_information[0]}\nИмя: {student_information[1]}\nФамилия: {student_information[2]}\nОтчество: {student_information[3]}\nГруппа: {student_information[4]}\nОценки: {grades}\nСредний балл: {sum(grades) / len(grades)}"
 
-    def update_student(self,student_id,student):
-        grades_str = ','.join(map(str, student.grades))
-        self.cursor.execute('''
-                    UPDATE students
-                    SET first_name = ?, last_name = ?, middle_name = ?, group_name = ?, grades = ?
-                    WHERE id = ?
-                ''', (student.first_name, student.last_name, student.middle_name, student.group, grades_str,
-                      student_id))
-        self.con.commit()
+def update_student(student_id, student):
+    grades_str = ','.join(map(str, student.grades))
+    cursor.execute('''
+                UPDATE students
+                SET first_name = ?, last_name = ?, middle_name = ?, group_name = ?, grades = ?
+                WHERE id = ?
+            ''', (student.first_name, student.last_name, student.middle_name, student.group, grades_str, student_id))
+    con.commit()
 
-    def delete_student(self, student_id):
-        self.cursor.execute('DELETE FROM students WHERE id = ?', (student_id,))
-        self.con.commit()
+def delete_student(student_id):
+    cursor.execute('DELETE FROM students WHERE id = ?', (student_id,))
+    con.commit()
 
-    def get_average_grade_group(self, group_name):
-        self.cursor.execute('SELECT grades FROM students WHERE group_name = ?', (group_name,))
-        students_grades = self.cursor.fetchall()
-        all_grades = [int(grade) for student in students_grades for grade in student[0].split(',')]
-        return sum(all_grades) / len(all_grades)
-
-database = Database()
+def get_average_grade_group(group_name):
+    cursor.execute('SELECT grades FROM students WHERE group_name = ?', (group_name,))
+    students_grades = cursor.fetchall()
+    all_grades = [int(grade) for student in students_grades for grade in student[0].split(',')]
+    return sum(all_grades) / len(all_grades)
 
 while True:
     choice = int(input("1 - Добавление нового студента\n2 - Просмотр всех студентов\n3 - Просмотр одного студента, включая его средний балл\n4 - Редактирование студента\n5 - Удаление студента\n6 - Просмотр среднего балла студентов у конкретной группы\n7 - Завершить\nДействие: "))
@@ -76,26 +69,29 @@ while True:
         group = input("Введите группу: ")
         grades_input = input("Введите оценки через запятую (5,5,5,5): ")
         grades = list(map(int, grades_input.split(',')))
-        student = Student(first_name,last_name,middle_name,group,grades)
-        database.add_student(student)
+        student = Student(first_name, last_name, middle_name, group, grades)
+        add_student(student)
         print("Студент создан")
+
     elif choice == 2:
-        all_students = database.get_all_students()
+        all_students = get_all_students()
         print("Список студентов:")
         for i in all_students:
-            print(f"ID: {i[0]}, Имя: {i[1]}, Фамилия: {i[2]},Отчество: {i[3]} Группа: {i[4]}")
+            print(f"ID: {i[0]}, Имя: {i[1]}, Фамилия: {i[2]}, Отчество: {i[3]}, Группа: {i[4]}")
+
     elif choice == 3:
-        all_students = database.get_all_students()
+        all_students = get_all_students()
         for i in all_students:
             print(f"ID: {i[0]}, Имя: {i[1]}, Фамилия: {i[2]}, Группа: {i[4]}")
         student_id = int(input("Введите ID студента: "))
-        student_info = database.get_student(student_id)
+        print(get_student(student_id))
+
     elif choice == 4:
-        all_students = database.get_all_students()
+        all_students = get_all_students()
         for i in all_students:
             print(f"ID: {i[0]}, Имя: {i[1]}, Фамилия: {i[2]}, Группа: {i[4]}")
         student_id = int(input("Введите ID студента, которого хотите отредактировать: "))
-        student_info = database.get_student(student_id)
+        print(get_student(student_id))
         print("Введите новые данные для студента:")
         first_name = input("Введите имя: ")
         last_name = input("Введите фамилию: ")
@@ -104,17 +100,20 @@ while True:
         grades_input = input("Введите оценки через запятую (5,5,5,5): ")
         grades = list(map(int, grades_input.split(',')))
         updated_student = Student(first_name, last_name, middle_name, group, grades)
-        database.update_student(student_id, updated_student)
-        updated_student_info = database.get_student(student_id)
+        update_student(student_id, updated_student)
+        print(get_student(student_id))
+
     elif choice == 5:
-        all_students = database.get_all_students()
+        all_students = get_all_students()
         for i in all_students:
             print(f"ID: {i[0]}, Имя: {i[1]}, Фамилия: {i[2]}, Группа: {i[4]}")
         student_id = int(input("Введите ID студента, которого хотите удалить: "))
-        database.delete_student(student_id)
+        delete_student(student_id)
         print("Студент удалён")
+
     elif choice == 6:
         group_name = input("Введите номер группы: ")
-        print(f"Средний балл всей группы: {database.get_average_grade_group(group_name)}")
+        print(f"Средний балл всей группы: {get_average_grade_group(group_name)}")
+
     elif choice == 7:
         break
